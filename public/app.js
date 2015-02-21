4efc8ee1f5e33e9cb45b9e056ad9988c9bbd2ee1
Mike Hockerman <mike@hockerman.com>
2015-02-21 17:48:57 -0500

    Update app.js

--- public/app.js
+++ public/app.js
@@ -3,23 +3,24 @@ var app = angular.module('chatApp', ['firebase'])
 
 app.controller('ChatCtrl', ['$scope', '$firebase', function($scope, $firebase) {
 	
-	var baseRef = new Firebase("https://anvil-demo.firebaseio.com/")
 	var msgRef = new Firebase("https://anvil-demo.firebaseio.com/messages")
-	
 	var sync = $firebase(msgRef)
-
 	$scope.messages = sync.$asArray()
 
+	$scope.addMessage = function(text) {
+		var message = {
+			sender: $scope.authData.facebook.displayName,
+			text: text
+		}
+		$scope.messages.$add(message)
+		$scope.newMessageText = ""
+	}
+
 	$scope.signin = function() {
-		baseRef.authWithOAuthPopup('facebook', function(error, authData) {
+		msgRef.authWithOAuthPopup('facebook', function(error, authData) {
 			$scope.authData = authData
 			$scope.$digest()
 		})
-	}
-
-	$scope.addMessage = function(text) {
-		$scope.messages.$add( { sender: $scope.authData.facebook.displayName, text: text } )
-		$scope.newMessageText = ""
 	}
 
 }])
